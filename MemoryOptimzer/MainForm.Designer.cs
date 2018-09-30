593503df0e2a6851e0db924c77dfed4b3666dc24
Bruce Wayne <admin@bige0.com>
2018-09-30 17:22:07 +0800

    Init demo

--- MemoryOptimzer/MainForm.Designer.cs
+++ MemoryOptimzer/MainForm.Designer.cs
@@ -28,20 +28,99 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.progressBar2 = new System.Windows.Forms.ProgressBar();
+			this.label3 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.button1.Location = new System.Drawing.Point(144, 99);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "负优化";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.progressBar1.Location = new System.Drawing.Point(14, 24);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(205, 23);
+			this.progressBar1.TabIndex = 1;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(65, 12);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "内存使用：";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 55);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(41, 12);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "进度：";
+			// 
+			// progressBar2
+			// 
+			this.progressBar2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.progressBar2.Location = new System.Drawing.Point(14, 70);
+			this.progressBar2.Name = "progressBar2";
+			this.progressBar2.Size = new System.Drawing.Size(205, 23);
+			this.progressBar2.Step = 1;
+			this.progressBar2.TabIndex = 4;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(12, 104);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(77, 12);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "成功负优化：";
 			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(231, 131);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.progressBar2);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.progressBar1);
+			this.Controls.Add(this.button1);
+			this.MaximumSize = new System.Drawing.Size(9999, 170);
+			this.MinimumSize = new System.Drawing.Size(16, 170);
 			this.Name = "MainForm";
 			this.Text = "MemoryOptimzer";
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.ProgressBar progressBar1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.ProgressBar progressBar2;
+		private System.Windows.Forms.Label label3;
 	}
 }
 
--- MemoryOptimzer/MainForm.cs
+++ MemoryOptimzer/MainForm.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +11,97 @@ namespace MemoryOptimzer
 		public MainForm()
 		{
 			InitializeComponent();
+		}
+
+		private delegate void VoidMethod_Delegate();
+
+		[DllImport(@"psapi.dll")]
+		private static extern int EmptyWorkingSet(IntPtr hwProc);
+
+		private void ClearMemory()
+		{
+			GC.Collect();
+			GC.WaitForPendingFinalizers();
+			var processes = Process.GetProcesses();
+			SetprogressBar2Max(processes.Length);
+			var succeed = 0;
+			foreach (var process in processes)
+			{
+				try
+				{
+					if (process.ProcessName == @"System" && process.ProcessName == @"Idle")
+					{
+						continue;
+					}
+
+					EmptyWorkingSet(process.Handle);
+					++succeed;
+				}
+				catch
+				{
+					//ignore
+				}
+				finally
+				{
+					StepprogressBar2();
+					SetLabel3($@"成功负优化：{succeed}/{processes.Length}");
+				}
+			}
+		}
+
+		private void SetprogressBar2(int value)
+		{
+			progressBar2.Invoke(new VoidMethod_Delegate(() =>
+			{
+				progressBar2.Value = value;
+			}));
+		}
+
+		private void SetprogressBar1(int value)
+		{
+			progressBar1.Invoke(new VoidMethod_Delegate(() =>
+			{
+				progressBar1.Value = value;
+			}));
+		}
+
+		private void StepprogressBar2()
+		{
+			progressBar2.Invoke(new VoidMethod_Delegate(() =>
+			{
+				progressBar2.PerformStep();
+			}));
+		}
+
+		private void SetprogressBar2Max(int value)
+		{
+			progressBar2.Invoke(new VoidMethod_Delegate(() =>
+			{
+				progressBar2.Value = 0;
+				progressBar2.Maximum = value;
+			}));
+		}
+
+		private void SetLabel3(string str)
+		{
+			label3.Invoke(new VoidMethod_Delegate(() =>
+			{
+				label3.Text = str;
+			}));
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			button1.Enabled = false;
+			var t = new Task(ClearMemory);
+			t.Start();
+			t.ContinueWith(task =>
+			{
+				button1.BeginInvoke(new VoidMethod_Delegate(() =>
+				{
+					button1.Enabled = true;
+				}));
+			});
 		}
 	}
 }
